92937d0aea00d2ddeaacef603571dc4e6c42c335
marioBurgosLed <mario.burgos.led@gmail.com>
2017-08-23 16:14:41 -0300

    Initial commit

---
+++ app/src/main/java/com/example/note/recyclearview02/ListItem.java
@@ -0,0 +1,26 @@
+package com.example.note.recyclearview02;
+
+/**
+ * Created by Note on 23/08/2017.
+ */
+
+public class ListItem {
+
+    private String Head;
+    private String Desc;
+
+    public ListItem(String Head, String Desc) {
+        this.Head = Head;
+        this.Desc = Desc;
+    }
+
+    public String getHead() {
+        return Head;
+    }
+
+    public String getDesc() {
+        return Desc;
+    }
+
+
+}
